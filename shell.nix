5bc7451f43be3635ee7714059222b8dcbe029825
Benjamin Pannell <benjamin@pannell.dev>
2022-11-08 22:33:24 +0000

    feat: Add a dev shell in nix

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "moz_overlay_shell";
+    buildInputs = [
+      # to use the latest nightly:
+      nixpkgs.latest.rustChannels.nightly.rust
+      nixpkgs.libiconv
+      nixpkgs.protobuf
+    ]
+    ++ lib.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.Security];
+
+    PROTOC = "${pkgs.protobuf}/bin/protoc";
+  }
