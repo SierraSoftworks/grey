55f75ceb71e970bee3f419d430394c5b942a1cd4
Benjamin Pannell <benjamin@pannell.dev>
2025-08-11 09:06:36 +0100

    feat: Add humantime dependency and enhance probe component with formatted policy display

--- ui/src/components/probe.rs
+++ ui/src/components/probe.rs
@@ -9,23 +9,28 @@ pub struct ProbeProps {
 
 #[function_component(Probe)]
 pub fn probe(props: &ProbeProps) -> Html {
-    let probe_class = if props.probe.availability == 100.0 {
-        "ok"
-    } else if props.probe.availability > 90.0 {
-        "warn"
-    } else {
-        "error"
+    let recent_availability = props.history.iter().filter(|h| h.pass).count() as f64 / props.history.len() as f64;
+    let probe_class = match props.history.last() {
+        Some(h) if h.pass => "ok",
+        Some(h) if !h.pass && recent_availability > 0.8 => "warn",
+        Some(h) if !h.pass && recent_availability <= 0.8 => "error",
+        _ => "ok",
     };
+
+    let policy = format!("interval: {}, timeout: {}, retries: {}",
+        humantime::format_duration(props.probe.policy.interval),
+        humantime::format_duration(props.probe.policy.timeout),
+        props.probe.policy.retries.unwrap_or(0));
 
     html! {
         <div class={format!("section probe {}", probe_class)}>
             <h3>
                 {&props.probe.name}
-                <span class="availability">{format!("{}%", props.probe.availability)}</span>
+                <span class="availability">{format!("{:.3}%", props.probe.availability)}</span>
             </h3>
             <div class="probe-config probe-target">
                 <span>{&props.probe.target}</span>
-                <span>{format!("interval: {}ms", props.probe.policy.interval.as_millis())}</span>
+                <span>{policy}</span>
             </div>
             <History samples={props.history.clone()} />
         </div>
